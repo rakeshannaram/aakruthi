487c0ad03127c55ac448763a1313d9faa910df10
rakeshannaram <rakeshannaamjee@gmail.com>
2022-05-30 12:35:26 +0530

    uploading aakruthi files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+// import About from './About'
+// import Contact from './Contact'
+import Home from './Home'
+import Navbar from './Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Router>
+            <Navbar/>
+            <Routes>
+                {/* <Route exact path="/about" element={<About />}>
+                
+                </Route>
+                <Route exact path="/contact" element={<Contact />}>
+                
+                </Route> */}
+                <Route exact path="/" element={<Home />}>
+                
+                </Route>
+                
+            </Routes>
+            
+        </Router>
   );
 }
 
